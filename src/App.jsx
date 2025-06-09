d7f469077209613d305385f889a28147b7f0b1de
Sasori Morningstar <ilyes.medjedoub@etu.usthb.dz>
2025-06-09 02:20:08 +0100

    Added some styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import deepMindsLogo from '/deepminds.svg'
 import './App.css'
 
 function App() {
@@ -10,13 +10,13 @@ function App() {
     <>
       <div>
         <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
+          <img src={deepMindsLogo} className="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Raqmen's deployment test @ DeepMinds</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
